55283697bf9548d98dbb3948f6e7464d66b48e5c
KyungminLee <kyungmin.lee.dev@gmail.com>
2020-12-02 12:21:27 +0900

    Feat : 텍스트필드 외 영역 터치하면 키보드 사라지는 기능 추가
    - Target-Action 패턴사용

--- SignUpFlow/SignUpFlow/ViewControllers/LoginViewController.swift
+++ SignUpFlow/SignUpFlow/ViewControllers/LoginViewController.swift
@@ -11,7 +11,20 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupGestureRecognizer()
+    }
     
+    @IBAction func tappedView(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
     
+    func setupGestureRecognizer() {
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tappedView(_:)))
+        
+        view.addGestureRecognizer(tapGesture)
+    }
 }
 
